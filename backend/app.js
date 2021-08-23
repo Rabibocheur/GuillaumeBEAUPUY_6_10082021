9d74fbce2e561cab11969db8e096c1610e0bb1be
Guillaume beaupuy <beaupuyg@outlook.fr>
2021-08-23 17:46:48 +0200

    modification port

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const mongoose = require("mongoose");
 const path = require("path");
+const helmet = require("helmet");
 
 require("dotenv").config();
 
@@ -33,6 +34,8 @@ const userRoutes = require("./routes/user");
 const sauceRoutes = require("./routes/sauce");
 
 app.use(express.json());
+
+app.use(helmet());
 
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/api/auth", userRoutes);
